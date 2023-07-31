69273e0d450c9a31e0bd02174374a1a16e77f3fd
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-07-31 14:34:51 -0300

    font size

--- kal-calc/src/components/Header/index.js
+++ kal-calc/src/components/Header/index.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.css"
 function Header() {
     return(
         <>
-        <h1 className={styles.header}>EM QUAL DUNGEON EU JOGO?</h1>
+        <h1 className={styles.h1}>EM QUAL DUNGEON EU JOGO?</h1>
         <h2 className={styles.h2}>CASO ESTEJA SOZINHO, COLOQUE SEU LEVEL NOS DOIS CAMPOS</h2>
         </>
     );
