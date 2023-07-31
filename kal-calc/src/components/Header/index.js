d7058f0197ee1d639f84269f67a7a1c08206efd9
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-07-31 15:56:49 -0300

    upgrade css

--- kal-calc/src/components/Header/index.js
+++ kal-calc/src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header() {
     return(
         <>
         <h1 className={styles.h1}>EM QUAL DUNGEON EU JOGO?</h1>
-        <h2 className={styles.h2}>CASO ESTEJA SOZINHO, COLOQUE SEU LEVEL NOS DOIS CAMPOS</h2>
+        <h2 className={styles.h2}>SE FOR SOLO, SEU LEVEL VAI NOS DOIS CAMPOS</h2>
         </>
     );
 }
