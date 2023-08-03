95f44880d935049b941b658dd360b46f3507bfbe
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-08-02 22:35:38 -0300

    Update index.js

--- kal-calc/src/components/Footer/index.js
+++ kal-calc/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css"
 
 function Footer() {
     return(
-            <h2 className={styles.footer}>Kal-Calc &copy; Desenvolvido por Alex Ponciano - 2023</h2>
+            <h2 className={styles.footer}>kalguide &copy; Desenvolvido por Alex Ponciano - 2023</h2>
     );
 }
 
